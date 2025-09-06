4faf7083b9f71d4431ba4232f7b885017008c5fa
cristian <cristianpulga13@gmail.com>
2025-09-06 07:45:15 -0500

    connected api openmeteo

--- src/components/GetApi.tsx
+++ src/components/GetApi.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { fetchWeatherApi } from 'openmeteo';
 
-export const GetApi = () => {
-  return (
-    <>
-      
-    </>
-  )
+const params = {
+	"latitude": 52.52,
+	"longitude": 13.41,
+	"hourly": "temperature_2m",
+};
+
+export const GetApi = async ():Promise<any> => {
+
+    const baseUrl = `https://api.open-meteo.com/v1/forecast`;
+    
+    const responses = await fetchWeatherApi(baseUrl, params);
+    const response = responses[0];
+
+    const latitude = response.latitude();
+    const longitude = response.longitude();
+    const elevation = response.elevation();
+    const utcOffsetSeconds = response.utcOffsetSeconds();
+
+    
+
+    const hourly = response.hourly()!;
+
+    const weatherData = {
+	hourly: {
+		time: [...Array((Number(hourly.timeEnd()) - Number(hourly.time())) / hourly.interval())].map(
+			(_, i) => new Date((Number(hourly.time()) + i * hourly.interval() + utcOffsetSeconds) * 1000)
+		),
+		temperature_2m: hourly.variables(0)!.valuesArray(),
+	},
+};
+
+console.log("\nHourly data", weatherData.hourly)
+
+    // const {image} = await response.json();
+    // return image;
+
+
 }
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { Interface } from './components/Interface'
+import { GetApi } from './components/GetApi'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     
-    <Interface/>
+    <GetApi/>
 
   </StrictMode>,
 )
